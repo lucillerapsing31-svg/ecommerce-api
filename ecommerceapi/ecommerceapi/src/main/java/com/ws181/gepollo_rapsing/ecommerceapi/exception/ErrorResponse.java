dfaa46cdb79375cb699baf1023febd040e4cca06
Gepollo Rapsing <gepollo.rapsing@example.com>
2026-04-22 07:55:27 +0800

    Added missing exception classes

---
+++ ecommerceapi/ecommerceapi/src/main/java/com/ws181/gepollo_rapsing/ecommerceapi/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.ws181.gepollo_rapsing.ecommerceapi.exception;
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
