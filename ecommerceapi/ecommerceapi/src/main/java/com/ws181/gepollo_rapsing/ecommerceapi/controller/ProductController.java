6f220abe956c3ac72b1a5a26140b2fe60a6568e1
lucillerapsing31-svg <lucillerapsing31@gmail.com>
2026-04-29 17:52:11 +0800

    Add CrossOrigin annotation to ProductController

--- ecommerceapi/ecommerceapi/src/main/java/com/ws181/gepollo_rapsing/ecommerceapi/controller/ProductController.java
+++ ecommerceapi/ecommerceapi/src/main/java/com/ws181/gepollo_rapsing/ecommerceapi/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
-@CrossOrigin(origins = "*") // 👈 ADD THIS LINE HERE
+@CrossOrigin(origins = "*") // ADD THIS LINE HERE
 public class ProductController {
 
     private final ProductService productService;
